48266a3a2d9d35a4d4377810a17009f3bbfbc838
Tony Komar <tony.komar@siemens.com>
2025-05-18 12:53:30 +0000

    Defined the tools as 1.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="capella_tools",
-    version="0.1",
+    version="1.0",
     packages=find_packages(),
     install_requires=[
         "capellambse",  # Include other dependencies if needed
